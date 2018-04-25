3d2f9f94b3e895a0cdad7172d8777bfa313d36b7
Tumelo Lephadi <lphtum003@sl-dual-150.cs.uct.ac.za>
2018-04-25 21:24:32 +0200

    defined main method yet to be tested

--- src/imageops.cpp
+++ src/imageops.cpp
@@ -1,16 +1,37 @@
 #include "image.h"
 
 int main(int argc, const char* argv[]){
-	switch(argv[0][1]){
+
+	image i1, i2, i3;
+
+	switch(argv[1][2]){
 		case 'a':
+			i1.load(argv[2]);
+			i2.load(argv[3]);
+			i3 = i1 + i2;
+			i3.save(argv[4]);
 		break;
 		case 's':
+			i1.load(argv[2]);
+			i2.load(argv[3]);
+			i3 = i1 - i2;
+			i3.save(argv[4]);
 		break;
 		case 'i':
+			i1.load(argv[1]);
+			i2 = !(i1);
+			i2.save(argv[3]);
 		break;
 		case 'l':
+			i1.load(argv[2]);
+			i2.load(argv[3]);
+			i3 = i1 / i2;
+			i3.save(argv[4]);
 		break;
 		case 't':
+			i1.load(argv[2]);
+			i2 = i1 * atoi(argv[3]);
+			i2.save(argv[4]);
 		break;
 	}
 	return 0;
