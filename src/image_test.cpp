7a7907baca25b8503fb043d37e9bb28f0da87f5d
Tumelo Lephadi <lphtum003@sl-dual-150.cs.uct.ac.za>
2018-04-27 01:56:13 +0200

    tested save

--- src/image_test.cpp
+++ src/image_test.cpp
@@ -37,3 +37,19 @@ TEST_CASE("void load(const std::string& filename)"){
 	REQUIRE(i1.getWidth() == 1207);
 	REQUIRE(i1.getHeight() == 1280);
 }
+
+TEST_CASE("void save(const std::string& filename)"){
+	image i1;
+	i1.load("../pics/shrek_rectangular.pgm");
+	image i2 = i1;
+	i2.save("../pics/shrek_rectangular_save.pgm");
+}
+
+TEST_CASE("image& operator=(const image& lhs)"){
+	image i1;
+	i1.load("../pics/shrek_rectangular.pgm");
+	image i2 = i1;
+
+	REQUIRE(i2.getWidth() == 1207);
+	REQUIRE(i2.getHeight() == 1280);
+}
