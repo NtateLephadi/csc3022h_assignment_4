4451b65979b45aa4291fc9b90f6717c54af4cdaf
Tumelo Lephadi <lphtum003@sl-dual-150.cs.uct.ac.za>
2018-04-27 01:58:05 +0200

    tested move assignment operator

--- src/image_test.cpp
+++ src/image_test.cpp
@@ -53,3 +53,15 @@ TEST_CASE("image& operator=(const image& lhs)"){
 	REQUIRE(i2.getWidth() == 1207);
 	REQUIRE(i2.getHeight() == 1280);
 }
+
+TEST_CASE("image& operator=(image&& rhs)"){
+	image i1;
+	i1.load("../pics/shrek_rectangular.pgm");
+	image i2 = std::move(i1);
+
+	REQUIRE(i1.getWidth() == 0);
+	REQUIRE(i1.getHeight() == 0);
+
+	REQUIRE(i2.getWidth() == 1207);
+	REQUIRE(i2.getHeight() == 1280);
+}
