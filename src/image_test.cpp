daaf8c6bdd13c8086ab9866c9eb306ef1071ac2d
Tumelo Lephadi <lphtum003@sl-dual-150.cs.uct.ac.za>
2018-04-27 01:49:30 +0200

    tested move and copy constructor in image

--- src/image_test.cpp
+++ src/image_test.cpp
@@ -3,6 +3,7 @@
 
 TEST_CASE( "image()" ){
 	image i = image();
+
 	REQUIRE(i.getWidth() == 0);
 	REQUIRE(i.getHeight() == 0);
 	REQUIRE(i.getData() == nullptr);
@@ -10,8 +11,21 @@ TEST_CASE( "image()" ){
 
 TEST_CASE("image(const image& rhs);"){
 	image i1;
-	i1.load("../test/pics/shrek_rectangular.pgm");
+	i1.load("../pics/shrek_rectangular.pgm");
 	image i2(i1);
-	REQUIRE(i1.getWidth() == i2.getWidth());
-	REQUIRE(i1.getHeight() == i2.getHeight());
+
+	REQUIRE(i2.getWidth() == 1207);
+	REQUIRE(i2.getHeight() == 1280);
+}
+
+TEST_CASE("image(image&& rhs)"){
+	image i1;
+	i1.load("../pics/shrek_rectangular.pgm");
+	image i2 = std::move(i1);
+
+	REQUIRE(i1.getWidth() == 0);
+	REQUIRE(i1.getHeight() == 0);
+
+	REQUIRE(i2.getWidth() == 1207);
+	REQUIRE(i2.getHeight() == 1280);
 }
